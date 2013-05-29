48845f335cda2cce5b2bd00e4dd198d099df7d97
Shu Sugimoto <shu@su.gimo.to>
2013-05-29 09:57:34 +0900

    plugin defaults

---
+++ init/code.ini.php
@@ -0,0 +1,10 @@
+<?php
+define("PLUGIN_CODE_LANGUAGE", 'pre');
+define("PLUGIN_CODE_NUMBER",    0);
+define("PLUGIN_CODE_OUTLINE",   1);
+define('PLUGIN_CODE_BLOCK',     1);
+define('PLUGIN_CODE_LITERAL',   1);
+define('PLUGIN_CODE_COMMENT',   0);
+define("PLUGIN_CODE_MENU",      1);
+define("PLUGIN_CODE_FILE_ICON", 1);
+?>
